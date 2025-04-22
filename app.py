60fc6783a548b860d49b1ed1c81bb03a51bc1db3
Fernando <fernando.decalle@gmail.com>
2025-04-22 20:14:12 +0200

    added s3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 df = pd.read_html(
     "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html",
@@ -7,3 +8,7 @@ df = pd.read_html(
 df = df[0].iloc[:, [0, 2]]
 
 print(df)
+
+bucket = 'miax13-project-1'
+current_date = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+df.to_csv(f"s3://{bucket}/ccy/{current_date}.csv")
